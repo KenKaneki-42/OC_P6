93febeb08c4f0a759105669b43f3bca398a906b5
Sylvain Vandermeersch <sylvain.vandermeersch@gmail.com>
2024-04-05 17:17:22 +0200

    new migration for TrickCategory

---
+++ migrations/Version20240405135307.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240405135307 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE trick_category (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE trick ADD trick_category_id INT NOT NULL, DROP category');
+        $this->addSql('ALTER TABLE trick ADD CONSTRAINT FK_D8F0A91E9C71FA8B FOREIGN KEY (trick_category_id) REFERENCES trick_category (id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D8F0A91E5E237E06 ON trick (name)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D8F0A91E989D9B62 ON trick (slug)');
+        $this->addSql('CREATE INDEX IDX_D8F0A91E9C71FA8B ON trick (trick_category_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE trick DROP FOREIGN KEY FK_D8F0A91E9C71FA8B');
+        $this->addSql('DROP TABLE trick_category');
+        $this->addSql('DROP INDEX UNIQ_D8F0A91E5E237E06 ON trick');
+        $this->addSql('DROP INDEX UNIQ_D8F0A91E989D9B62 ON trick');
+        $this->addSql('DROP INDEX IDX_D8F0A91E9C71FA8B ON trick');
+        $this->addSql('ALTER TABLE trick ADD category JSON DEFAULT NULL, DROP trick_category_id');
+    }
+}
