debd49187049ab7eada9d67845101571f4dabd49
Sylvain Vandermeersch <sylvain.vandermeersch@gmail.com>
2024-04-09 15:07:05 +0200

    adapt RegistrationFormType

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -11,45 +12,55 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class RegistrationFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options): void
-    {
-        $builder
-            ->add('email')
-            ->add('agreeTerms', CheckboxType::class, [
-                'mapped' => false,
-                'constraints' => [
-                    new IsTrue([
-                        'message' => 'You should agree to our terms.',
-                    ]),
-                ],
-            ])
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
-                ],
-            ])
-        ;
-    }
+  public function buildForm(FormBuilderInterface $builder, array $options): void
+  {
+    $builder
+      ->add('firstname', TextType::class, ['label' => 'Nom'])
+      ->add('lastname', TextType::class, ['label' => 'Prénom'])
+      ->add('email', EmailType::class)
+      ->add('plainPassword', RepeatedType::class, [
+        'type' => PasswordType::class,
+        'first_options' => [
+          'attr' => ['autocomplete' => 'new-password'],
+          'constraints' => [
+            new NotBlank([
+              'message' => 'Please enter a password',
+            ]),
+            new Length([
+              'min' => 6,
+              'minMessage' => 'Your password should be at least {{ limit }} characters',
+              'max' => 4096,
+            ]),
+          ],
+          'label' => 'Mot de passe',
+        ],
+        'second_options' => [
+          'attr' => ['autocomplete' => 'new-password'],
+          'label' => 'Confirmation de mot de passe',
+        ],
+        'invalid_message' => 'The password fields must match.',
+      ])
+      ->add('agreeTerms', CheckboxType::class, [
+        'label' => 'Accepter les conditions',
+        'mapped' => false,
+        'constraints' => [
+          new IsTrue([
+            'message' => 'You should agree to our terms.',
+          ]),
+        ],
+      ]);
+  }
 
-    public function configureOptions(OptionsResolver $resolver): void
-    {
-        $resolver->setDefaults([
-            'data_class' => User::class,
-        ]);
-    }
+  public function configureOptions(OptionsResolver $resolver): void
+  {
+    $resolver->setDefaults([
+      'data_class' => User::class,
+      'validation_groups', ['password', 'Default']
+    ]);
+  }
 }
