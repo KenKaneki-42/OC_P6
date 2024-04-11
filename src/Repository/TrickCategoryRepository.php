d7762126e24c8d2226513c4bea7e83ddd5880acd
Sylvain Vandermeersch <sylvain.vandermeersch@gmail.com>
2024-04-11 12:11:43 +0200

    remove unused code

--- src/Repository/TrickCategoryRepository.php
+++ src/Repository/TrickCategoryRepository.php
@@ -21,28 +21,4 @@ class TrickCategoryRepository extends ServiceEntityRepository
         parent::__construct($registry, TrickCategory::class);
     }
 
-    //    /**
-    //     * @return TrickCategory[] Returns an array of TrickCategory objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('t')
-    //            ->andWhere('t.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('t.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?TrickCategory
-    //    {
-    //        return $this->createQueryBuilder('t')
-    //            ->andWhere('t.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
 }
