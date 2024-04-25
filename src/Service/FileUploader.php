2a54cd9e135196f80fcf3a20961461d836bc9930
Sylvain Vandermeersch <sylvain.vandermeersch@gmail.com>
2024-04-25 15:20:09 +0200

    improve FileUploader

--- src/Service/FileUploader.php
+++ src/Service/FileUploader.php
@@ -9,60 +9,68 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 
 class FileUploader
 {
-    private $targetDirectory;
-    private $slugger;
+  private $targetDirectory;
+  private $slugger;
 
-    public function __construct(string $targetDirectory, SluggerInterface $slugger)
-    {
-        $this->targetDirectory = $targetDirectory;
-        $this->slugger = $slugger;
+  public function __construct(string $targetDirectory, SluggerInterface $slugger)
+  {
+    $this->targetDirectory = $targetDirectory;
+    $this->slugger = $slugger;
+  }
+
+  public function upload(UploadedFile $file): string
+  {
+    $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+    $safeFilename = $this->slugger->slug($originalFilename);
+    $fileName = $safeFilename . '-' . uniqid() . '.' . $file->guessExtension();
+
+    try {
+      $file->move($this->getTargetDirectory(), $fileName);
+    } catch (FileException $e) {
+      throw new FileException($e);
     }
 
-    public function upload(UploadedFile $file): string
-    {
-        $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
-        $safeFilename = $this->slugger->slug($originalFilename);
-        $fileName = $safeFilename.'-'.uniqid().'.'.$file->guessExtension();
+    return $fileName;
+  }
 
-        try {
-            $file->move($this->getTargetDirectory(), $fileName);
-        } catch (FileException $e) {
-            throw new FileException($e);
-        }
-
-        return $fileName;
+  public function uploadImages(Trick $trick): void
+  {
+    foreach ($trick->getImages() as $image) {
+      if ($image->getFile() !== null) {
+        $image->setName($this->upload($image->getFile()));
+        $image->setUrl($this->getTargetDirectory() . '/' . $image->getName());
+        $image->setDescription($image->getDescription());
+        $image->setSource('upload');
+        $image->setExternalId('none');
+      } elseif ($image->getName() === null && $image->getFile() === null) {
+        $trick->removeImage($image);
+      }
     }
+  }
 
-    public function uploadImages(Trick $trick): void
-    {
-        foreach ($trick->getImages() as $image) {
-            if ($image->getFile() !== null) {
-                $image->setName($this->upload($image->getFile()));
-            } elseif ($image->getName() === null && $image->getFile() === null) {
-                $trick->removeImage($image);
-            }
-        }
+  public function getTargetDirectory(): string
+  {
+    return $this->targetDirectory;
+  }
+
+
+  public function uploadVideos(Trick $trick): void
+  {
+    foreach ($trick->getVideos() as $video) {
+
+      $check = parse_url($video->getUrl(), PHP_URL_HOST);
+      parse_str(parse_url($video->getUrl(), PHP_URL_QUERY), $videoId);
+
+      if ($check === "www.youtube.com" && array_key_exists('v', $videoId)) {
+        $video->setexternalId($videoId['v']);
+        $video->setSource($check);
+        $video->setUrl($video->getUrl());
+        $video->setName($video->getName());
+        $video->setDescription($video->getDescription());
+        $trick->addVideo($video);
+      } elseif ($video->getName() === null || $video->getExternalId() === null) {
+        $trick->removeVideo($video);
+      }
     }
-
-    public function getTargetDirectory(): string
-    {
-        return $this->targetDirectory;
-    }
-
-
-    public function uploadVideos(Trick $trick): void
-    {
-        foreach ($trick->getVideos() as $video) {
-            $check = parse_url($video->getName(), PHP_URL_HOST) ;
-            parse_str(parse_url($video->getName(), PHP_URL_QUERY), $videoId);
-
-            if ($check === "www.youtube.com" && array_key_exists('v', $videoId)) {
-                $video->setexternalId($videoId['v']);
-
-                $trick->addVideo($video);
-            } elseif ($video->getName() === null || $video->getExternalId() === null) {
-                $trick->removeVideo($video);
-            }
-        }
-    }
+  }
 }
