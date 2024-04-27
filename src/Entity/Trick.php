44e4b3510e902cde8133a65536eea43766b01de6
Sylvain Vandermeersch <sylvain.vandermeersch@gmail.com>
2024-04-27 04:37:40 +0200

    change description property type from string to text

--- src/Entity/Trick.php
+++ src/Entity/Trick.php
@@ -21,7 +21,7 @@ class Trick
   #[ORM\Column(length: 255, unique: true)]
   private ?string $name = null;
 
-  #[ORM\Column(length: 255)]
+  #[ORM\Column(type: "text", nullable: false)]
   private ?string $description = null;
 
   #[ORM\Column]
